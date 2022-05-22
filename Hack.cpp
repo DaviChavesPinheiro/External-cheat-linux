5c8743f56e9e06fd99d7a03f9b008e551d98797f
DaviChavesPinheiro <davichavespinheiro011010@gmail.com>
2022-05-22 17:30:21 -0300

    [Heap]: heap address

--- Hack.cpp
+++ Hack.cpp
@@ -37,6 +37,28 @@ long findPidByName(string proc_name) {
   return -1;
 }
 
+int findHeapAddress(long pid, unsigned long* heap_s, unsigned long* heap_e) {
+  
+  // Gets maps file path
+  char maps_path[64];
+  sprintf(maps_path, "/proc/%ld/maps", pid);
+
+  // Read the file
+  ifstream maps(maps_path);
+  string line;
+
+  while(std::getline(maps, line)) {
+    if(line.find("[heap]") != line.npos) {
+      *heap_s = std::stoul(line.substr(0, 12), NULL, 16);
+      *heap_e = std::stoul(line.substr(13, 12), NULL, 16);
+      return 0;
+    }
+  }
+
+  return -1;
+}
+
+
 int main (int argc, char *argv[])
 {
   if(argc < 2) {
@@ -52,5 +74,14 @@ int main (int argc, char *argv[])
   }
 
   cout << "[Info]: Processo encontrado (" << pid << ")" << endl;
+
+  // Find heap start address
+  unsigned long heap_s = -1, heap_e = -1;
+  if (findHeapAddress(pid, &heap_s, &heap_e) == -1) {
+    cout << "[Error]: Heap address not found" << endl;
+    return 1;
+  }
+  printf("[Info]: Heap: 0x%lx - 0x%lx\n", heap_s, heap_e);
+  printf("[Info]: Heap: %lu bytes\n", heap_e - heap_s);
   return 0;
 }
